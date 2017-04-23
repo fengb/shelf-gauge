831c54e582237cffd57cd866e758b5bd79bbd5b6
Benjamin Feng <contact@fengb.me>
2017-04-23 11:51:07 -0500

    Create or update repository

--- lib/controller/repository.ts
+++ lib/controller/repository.ts
@@ -3,14 +3,11 @@ import Repository from 'lib/entity/repository'
 
 export async function create (ctx: Context) {
   const name = `${ctx.params.org}/${ctx.params.name}`
-  const existing = await ctx.conn.entityManager.findOne(Repository, { name })
-  if (existing) {
-    ctx.status = HttpStatus.UnprocessableEntity
-    return
-  }
+  const repo = await ctx.conn.entityManager.findOne(Repository, { name })
+               || ctx.conn.entityManager.create(Repository, { name })
 
-  const repo = new Repository({ name })
+  // TODO: update data
+  ctx.status = repo.id ? HttpStatus.Ok : HttpStatus.Created
   await ctx.conn.entityManager.persist(repo)
-  ctx.status = HttpStatus.Created
   ctx.body = repo
 }
