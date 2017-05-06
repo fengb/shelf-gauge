00f65f9942e099d01a26312f4f3a64910aa289da
Benjamin Feng <contact@fengb.me>
2017-05-06 15:25:18 -0500

    Sanity checks for beforeAll / afterAll

--- test/support/mocha.ts
+++ test/support/mocha.ts
@@ -13,21 +13,33 @@ declare module 'mocha' {
 
 type HookCallback = (this: Mocha.IHookCallbackContext) => Promise<any>
 
+let BEFORE_ALL_CALLED = false
+
 export function beforeAll (callback: HookCallback) {
+  if (BEFORE_ALL_CALLED) {
+    throw new Error('beforeAll() already triggered')
+  }
   beforeAllCallbacks.push(callback)
 }
 
+let AFTER_ALL_CALLED = false
+
 export function afterAll (callback: HookCallback) {
+  if (AFTER_ALL_CALLED) {
+    throw new Error('afterAll() already triggered')
+  }
   afterAllCallbacks.push(callback)
 }
 
 const beforeAllCallbacks = [] as HookCallback[]
 before(function () {
+  BEFORE_ALL_CALLED = true
   return Promise.map(beforeAllCallbacks, (cb) => cb.call(this))
 })
 
 const afterAllCallbacks = [] as HookCallback[]
 after(function () {
+  AFTER_ALL_CALLED = true
   return Promise.map(afterAllCallbacks, (cb) => cb.call(this))
 })
 
