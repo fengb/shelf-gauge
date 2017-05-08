81f2bdb0436d5fa6e72464414cc5386a36650739
Benjamin Feng <contact@fengb.me>
2017-05-07 21:59:28 -0500

    Test secure-random

--- lib/util/secure-random.ts
+++ lib/util/secure-random.ts
@@ -3,6 +3,13 @@ import * as crypto from 'crypto'
 export const bytes = Promise.promisify(crypto.randomBytes)
 
 export async function base64 (len: number) {
-  const array = await bytes(len)
-  return array.toString('base64').substr(0, len)
+  // Base64 encodes each set of three bytes into four bytes.
+  // So we only need to generate 3/4 of full random, rounded up
+  const buffer = await bytes(Math.ceil(len * 3/4))
+  return buffer.toString('base64').substr(0, len)
+}
+
+export async function hex (len: number) {
+  const buffer = await bytes(Math.ceil(len/2))
+  return buffer.toString('hex').substr(0, len)
 }
