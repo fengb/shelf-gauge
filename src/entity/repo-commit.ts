2cf8c8a7313aa4742e1bb34bcfb42aa7865cffe7
Benjamin Feng <contact@fengb.me>
2017-05-24 08:52:28 -0500

    Added unique index to commit ref+parent

--- src/entity/repo-commit.ts
+++ src/entity/repo-commit.ts
@@ -3,6 +3,7 @@ import * as Typeorm from 'typeorm'
 import { Repo } from '.'
 
 @Typeorm.Entity()
+@Typeorm.Index(['ref', 'parent'], { unique: true })
 export default class RepoCommit {
   constructor (attrs: Partial<RepoCommit> = {}) {
     Object.assign(this, attrs)
