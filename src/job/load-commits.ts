59059172c526e392bfd9aea615924954d1e0f97c
Benjamin Feng <contact@fengb.me>
2017-05-24 08:48:21 -0500

    Added test for upsert commits

--- src/job/load-commits.ts
+++ src/job/load-commits.ts
@@ -1,18 +1,41 @@
 import ENV from 'config/env'
 
-import { flatMap, partialRight } from 'lodash'
+import { map, keyBy, filter } from 'lodash'
 
 import * as github from 'src/service/github'
 import { Repo, RepoCommit } from 'src/entity'
 import { connect } from 'src/server/connection'
 
 export async function fromGithub (repo: Repo, ref?: string) {
-  const prom: any = await Promise.props({
-    conn: connect(),
-    response: github.fetchCommits(repo.name, ref),
-  })
-  const commits = github.toCommits(prom.response.data, repo)
-  await prom.conn.entityManager.persist(commits)
+  const response = await github.fetchCommits(repo.name, ref)
+  const commits = github.toCommits(response.data, repo)
+  return upsert(commits)
+}
+
+function hashify (commit: RepoCommit) {
+  return `${commit.ref}~${commit.parent}`
+}
+
+export async function upsert (commits: RepoCommit[]) {
+  const conn = await connect()
+  const dbCommits = await conn.entityManager
+                    .createQueryBuilder(RepoCommit, 'commit')
+                    .where('commit.ref IN (:refs)', {
+                      refs: map(commits, 'ref'),
+                    })
+                    .getMany()
+
+  const lookup = keyBy(dbCommits, hashify)
+  const needSaving = []
+  for (const commit of commits) {
+    const fromDb = lookup[hashify(commit)]
+    if (fromDb) {
+      commit.id = fromDb.id
+    } else {
+      needSaving.push(commit)
+    }
+  }
+  await conn.entityManager.persist(needSaving)
   return commits
 }
 
---
+++ test/job/load-commits.test.ts
@@ -0,0 +1,33 @@
+import { expect, db, factory } from 'test/support'
+
+import * as loadCommits from 'src/job/load-commits'
+import { RepoCommit } from 'src/entity'
+
+describe('job/load-commits', () => {
+  describe('upsert()', () => {
+    db.setup()
+
+    it('sets the id', async function () {
+      const commit = factory.repoCommit.build()
+      await loadCommits.upsert([commit])
+
+      expect(commit.id).to.be.ok
+    })
+
+    it('saves the commits', async function () {
+      const commit = factory.repoCommit.build()
+      await loadCommits.upsert([commit])
+
+      const fromDb = await this.conn!.entityManager.findOne(RepoCommit)
+      expect(fromDb).to.containSubset({ ref: commit.ref, parent: commit.parent })
+    })
+
+    it('reuses existing commits', async function () {
+      const existing = await factory.repoCommit.create()
+      const commit = factory.repoCommit.build({ ref: existing.ref, parent: existing.parent })
+      await loadCommits.upsert([commit])
+
+      expect(commit.id).to.equal(existing.id)
+    })
+  })
+})
