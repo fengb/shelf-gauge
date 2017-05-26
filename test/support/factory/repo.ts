739fd8cced217cddfa35f37d05137963394d8746
Benjamin Feng <contact@fengb.me>
2017-05-25 23:46:19 -0500

    Hardcode to github for now

--- test/support/factory/repo.ts
+++ test/support/factory/repo.ts
@@ -3,6 +3,7 @@ import { Repo } from 'src/entity'
 
 export default define(Repo, () => ({
   url: faker.internet.url,
-  source: randomize(...Repo.SOURCES),
+  source: randomize<Repo.Source>('github'),
+  //source: randomize(...Repo.SOURCES),
   name: () => faker.internet.domainWord() + '~' + faker.internet.domainWord(),
 }))
